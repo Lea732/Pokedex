9fe11d011e3e239951f9bfb815d747ca3771180c
Lea732 <leapoulin07@gmail.com>
2023-12-12 09:42:32 +0100

    Added first component

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,10 @@
+function PokemonCard() {
+  return (
+    <figure>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
+        <figcaption>Bulbasaur</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
