2eb8d94a9da7062a793a82e88a2fb1931ad18d88
Lea732 <leapoulin07@gmail.com>
2023-12-12 10:27:48 +0100

    Created components 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,31 @@
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  // Accéder au premier Pokémon du tableau pokemonList
+  const pokemon = pokemonList[1];
+
   return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>Bulbasaur</figcaption>
+      {/* Vérifier si l'image du Pokémon est disponible */}
+      {pokemon.imgSrc ? (
+        // Si oui, afficher l'image
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        // Sinon, afficher un paragraphe avec le texte "???"
+        <p>???</p>
+      )}
+      {/* Afficher le nom du Pokémon */}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
